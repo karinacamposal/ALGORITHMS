4b5f5cf940032a08cc35ddd6544baa694e502433
karinacamposal <2109020@upy.edu.mx>
2022-02-14 13:44:18 -0600

    estructura general C

--- unit1/generalStrucProgramC.c
+++ unit1/generalStrucProgramC.c
@@ -6,12 +6,14 @@
 //dataTypes in C/C++/C#PHP
 int unEntero = 3;
 float unDecimal = 3.1416;
-char* name = "KarinaCampos"
-char unCaracter = 'F'
+char* name = "KarinaCampos";
+char unCaracter = 'F';
 ///////////////////////////////
 
 
+/// La ejecución del programa///
 int main (){
-
+    printf("Hello Data B %s\n", name);  
     return 0;
 }
+////////////////////////////////////////
