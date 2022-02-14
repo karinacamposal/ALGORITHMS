f8fd3c68114de78c3f4815595b9e8953620273f9
karinacamposal <2109020@upy.edu.mx>
2022-02-14 12:57:15 -0600

    created some variables

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//*Global Variables*//;
+//Create a variable: dataType nameVariable = anyData;
+//dataTypes in C/C++/C#PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "KarinaCampos"
+char unCaracter = 'F'
+///////////////////////////////
+
+
+int main (){
+
+    return 0;
+}
