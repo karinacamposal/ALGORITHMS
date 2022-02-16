a6cb29c48ab04488407a1d0543f9b30e6aa64dfd
karinacamposal <2109020@upy.edu.mx>
2022-02-15 20:38:08 -0600

    condition

--- unit1/argv_argc.c
+++ unit1/argv_argc.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
-if (argc <= 1) return 0;
+    if (argc <= 1) return 0;
 
     printf("Program executed: %s, number of imputs elements: %d\n", argv [0], (argc-1));
-    printf("Hola %s %s %s, bienvenido!\n", argv[1], argv[2], argv[3],);
+    printf("Hola %s %s %s, bienvenida!\n", argv[1], argv[2], argv[3]);
     return 0;
 }
