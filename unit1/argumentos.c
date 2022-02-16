f8cbac09c4ce33470ab4a2f461f78080fcac5b59
karinacamposal <2109020@upy.edu.mx>
2022-02-15 19:04:48 -0600

    new files

---
+++ unit1/argumentos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main (){
+
+    int isGreater = isGreaterThan10(11);
+    printf ("the number is: %d", isGreater);
+    return 0;
+}
--- unit1/variablesGlobalLocal.c
+++ unit1/variablesGlobalLocal.c
@@ -10,8 +10,12 @@ char* date = "02-02-22T10:25:00z";
 int main(){
     date = "02-02-22T10:25:00z";
     int hours = 22; //Local:main
+    float pi = 3.1416; //%f
+    char character = 'c'; // %c
+    char* name = "Karinaaa"; //%s
+    int *adress = &hours;//%p
     hours = hours + 2;
-    printf("hours: %d\n", hours); 
+    printf("valor: %c," character); 
     return 0;
 }
 ////////////////////////////////////////
